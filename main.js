82b59e4d860333512d2958547f2a6aa07ebb99b7
VisionTan <tiling.tan@foxmail.com>
2019-08-06 04:11:17 +0800

    The first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+var n
+init()
+
+setInterval(() => { 
+    makeLeave(getImage(n))
+        .one('transitionend', (e) => { 
+            makeEnter($(e.currentTarget))
+        })
+    makeCurrent(getImage(n+1))
+    n = n + 1
+}, 1500)
+
+
+function getImage(n) { 
+    return $(`.image>img:nth-child(${x(n)})`)
+}
+function x(n) { 
+    if (n > 3) { 
+        n = n % 3
+        if (n === 0) { 
+            n=3
+        }
+    }
+    return n
+}
+
+function init() { 
+    n = 1
+    $(`.image>img:nth-child(${n})`).addClass('current')
+    .siblings().addClass('enter')
+}
+
+function makeCurrent($node) { 
+    return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node) { 
+    return $node.removeClass('enter current').addClass('leave')
+     
+}
+function makeEnter($node) { 
+    return $node.removeClass('current leave').addClass('enter')
+}
