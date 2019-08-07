9ce640527b181ea6a363b5c95a558f47fabf696e
VisionTan <tiling.tan@foxmail.com>
2019-08-08 04:05:39 +0800

    新增上下张切换和自动播放

--- main.js
+++ main.js
@@ -1,60 +1,83 @@
-var n
-init()
+var $buttons = $('#buttonWrapper > button')
+var $slides = $('#image')
+var $images = $slides.children('img')
+let current = 0
+makeFakeSlides()
 
-var timer = setInterval(() => { 
-    makeLeave(getImage(n))
-        .one('transitionend', (e) => { 
-            makeEnter($(e.currentTarget))
-        })
-    makeCurrent(getImage(n+1))
-    n = n + 1
-}, 1500)
+$slides.hide().offset()
+$slides.css({ transform: 'translateX(-400px)' }).show()
+bindEvent()
 
-document.addEventListener('visibilitychange', function () { 
-    if (document.hidden) { 
-        window.clearInterval(timer)
-    } else {
-        timer = setInterval(() => { 
-            makeLeave(getImage(n))
-                .one('transitionend', (e) => { 
-                    makeEnter($(e.currentTarget))
-                })
-            makeCurrent(getImage(n+1))
-            n = n + 1
-        }, 1500)
-    }
+$('#previous').on('click', function () { 
+    goToSlide(current-1)
+})
+$('#next').on('click', function () { 
+    goToSlide(current+1)
 })
 
-
-
-
-
-function getImage(n) { 
-    return $(`.image>img:nth-child(${x(n)})`)
-}
-function x(n) { 
-    if (n > 5) { 
-        n = n % 5
-        if (n === 0) { 
-            n=5
-        }
+var timer = setInterval(function(){
+    goToSlide(current+1)
+}, 2000)
+$(container).on('mouseenter', function () { 
+    window.clearInterval(timer)
+}).on('mouseleave', function () { 
+    timer = setInterval(function(){
+        goToSlide(current+1)
+    }, 2000)
+})
+document.addEventListener('visibilitychange', function () { 
+    if (document.hidden) {
+        window.clearInterval(timer)
+    } else { 
+        timer = setInterval(function(){
+            goToSlide(current+1)
+        }, 2000)
     }
-    return n
+})
+function bindEvent() { 
+
+    $('#buttonWrapper').on('click', 'button', function (e) { 
+        let $button = $(e.currentTarget)
+        let index = $button.index()
+        goToSlide(index)
+    })
+
 }
 
-function init() { 
-    n = 1
-    $(`.image>img:nth-child(${n})`).addClass('current')
-    .siblings().addClass('enter')
+function goToSlide(index) { 
+    if (index > $buttons.length-1) {
+        index = 0
+    } else if (index < 0) {
+        index = $buttons.length-1
+    }
+    if (current === $buttons.length - 1 && index === 0) {
+        //最后一张到第一张
+        $slides.css({
+            transform: `translateX(${-(current+2)*400}px)`
+        })
+            .one('transitionend', function () {
+                $slides.hide().offset()
+                $slides.css({ transform: `translateX(${-(index+1)*400}px)` }).show()
+            })
+    } else if (current === 0 && index === $buttons.length-1) {
+        //第一张到最后一张
+        $slides.css({
+            transform: `translateX(0)`
+        })
+        .one('transitionend', function () {
+            $slides.hide().offset()
+            $slides.css({ transform: `translateX(${-(current+1)*400}px)` }).show()
+        })
+    } else { 
+        $slides.css({transform:`translateX(${-(index+1)*400}px)`})
+    }
+    current = index
 }
 
-function makeCurrent($node) { 
-    return $node.removeClass('enter leave').addClass('current')
-}
-function makeLeave($node) { 
-    return $node.removeClass('enter current').addClass('leave')
-     
-}
-function makeEnter($node) { 
-    return $node.removeClass('current leave').addClass('enter')
+function makeFakeSlides() { 
+    var $firstCopy = $images.eq(0).clone(true)
+    var $lastCopy = $images.eq($images.length-1).clone(true)
+
+    $slides.append($firstCopy)
+    $slides.prepend($lastCopy)
 }
